c6316f226248dc9b23091dfbc8b71b3628333731
Nuno Freitas <nunofreitas@gmail.com>
2018-02-11 19:58:29 +0000

    build(webpack): create index.html

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,13 +1,28 @@
 import * as path from 'path';
 import CleanWebpackPlugin from 'clean-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import HtmlWebpackTemplate from 'html-webpack-template';
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'breakout.js'
+        filename: 'breakout.js',
     },
     plugins: [
-        new CleanWebpackPlugin(['dist'])
-    ]
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Breakout',
+            hash: true,
+            minify: {
+                collapseWhitespace: true,
+                preserveLineBreaks: true,
+                removeScriptTypeAttributes: true,
+            },
+            inject: false,
+            template: HtmlWebpackTemplate,
+            appMountId: 'game',
+            mobile: true,
+        }),
+    ],
 };
