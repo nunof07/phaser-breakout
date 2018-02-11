6a58788b149a857f3f1b9ab38f292468b85eb7fc
Nuno Freitas <nunofreitas@gmail.com>
2018-02-11 19:33:32 +0000

    build(webpack): cleanup dist folder before each build

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,9 +1,13 @@
 import * as path from 'path';
+import CleanWebpackPlugin from 'clean-webpack-plugin';
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'breakout.js'
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist'])
+    ]
 };
