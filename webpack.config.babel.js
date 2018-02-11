86f1ebc251117000af21c88e3643512e1464dc68
Nuno Freitas <nunofreitas@gmail.com>
2018-02-11 23:34:43 +0000

    build(webpack): setup server for development and watch for changes

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,13 +1,21 @@
 import * as path from 'path';
+import webpack from 'webpack';
 import CleanWebpackPlugin from 'clean-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import HtmlWebpackTemplate from 'html-webpack-template';
 
+const dist = path.resolve(__dirname, 'dist');
+
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: dist,
         filename: 'breakout.js',
+    },
+    devServer: {
+        contentBase: dist,
+        overlay: true,
+        hot: true,
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
@@ -24,5 +32,7 @@ module.exports = {
             appMountId: 'game',
             mobile: true,
         }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
     ],
 };
