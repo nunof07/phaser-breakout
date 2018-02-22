8635c36a57b6074a1c1949856c478b30ebce80b8
Nuno Freitas <nunofreitas@gmail.com>
2018-02-22 22:19:14 +0000

    build(fix build and improve performance):

--- webpack.prod.babel.js
+++ webpack.prod.babel.js
@@ -7,16 +7,20 @@ import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
 import LicenseBannerPlugin from 'license-banner-webpack-plugin';
 import common from './webpack.common.babel';
 
+const sourceMapEnabled = false;
+
 export default merge(common, {
     output: {
         filename: '[name].[chunkhash].js',
     },
-    devtool: 'source-map',
+    devtool: sourceMapEnabled ? 'source-map' : false,
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new webpack.HashedModuleIdsPlugin(),
         new UglifyJsPlugin({
-            sourceMap: true,
+            sourceMap: sourceMapEnabled,
+            cache: true,
+            parallel: true,
         }),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
